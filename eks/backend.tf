bc0618ac4733fd8038eaaa427abd2ff6bf21bed7
roshan074 <roshansahu6915@gmail.com>
2026-02-26 18:25:26 +0530

    Initial commit - EKS Terraform project

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "roshan-dev-tf-backend"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
